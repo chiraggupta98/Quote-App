c0e9a59d921209ae8c4d19a87b7325e2c996cd58
Chirag Gupta <chiragggupta007@gmail.com>
2025-03-23 13:10:21 +0530

    Add files via upload

---
+++ frontend/src/Components/pages/ShowQuotes.js
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
+const ShowQuotes = () => {
+    const params = useParams();
+
+    let [quote,setQuote] = useState({author:'',text:''})
+
+    async function fetchQuote(){
+       let res = await axios.get(`http://localhost:8080/quotes/${params.id}`)
+       let {author,text} = res.data;
+       console.log(author);
+       console.log(text);
+       setQuote({author,text});
+       console.log(quote);
+    }
+    useEffect(()=>{
+        fetchQuote();
+    },[])
+
+  return (
+    <div>
+        <p>{quote.text}</p>
+        <h2>{quote.author}</h2>
+    </div>
+  )
+}
+
+export default ShowQuotes
